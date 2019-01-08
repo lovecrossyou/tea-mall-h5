4c878d0732c75843c15cd2fb243013431b05e094
meng028 <15701664107@163.com>
2019-01-08 17:37:02 +0800

    home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,44 @@
 import React, { PureComponent } from 'react';
-import styles from './index.less';
+import { connect } from 'dva';
 import { TabBar } from 'antd-mobile';
+import { Carousel, WingBlank } from 'antd-mobile';
+import styles from './index.css';
 
-export default class Index extends PureComponent {
+import mall_classification from './image/mall_classification@2x.png';
+import mall_icon_search from './image/mall_icon_search@2x.png';
 
+
+class Index extends PureComponent{
   render() {
+    console.log(this.props.store);
     return (
-      <div>
-        店铺
+      <div className={styles.home_container}>
+        <div className={styles.home_header}>
+            <div className={styles.home_header_left}>
+              <img src={mall_classification} alt=""/>
+              <div>分类</div>
+            </div>
+            <div className={styles.home_header_right}>
+              <div className={styles.home_header_right_input}>
+                <img src={mall_icon_search} alt=""/>
+                <span>大家正在搜：喝茶时间表</span>
+              </div>
+            </div>
+        </div>
+        <div className={styles.carousel_container}>
+          <Carousel
+            autoplay={false}
+            infinite
+          >
+
+          </Carousel>
+        </div>
       </div>
     );
   }
 }
+export default connect(state=>{
+  return {
+    store:state.classify
+  }
+})(Index)
