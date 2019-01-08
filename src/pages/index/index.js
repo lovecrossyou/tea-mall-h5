eb5a14c6a11b64e7655014d1d8f880672a59375e
love1234 <345841634@qq.com>
2019-01-08 14:07:20 +0800

    xx

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import styles from './index.less'
 import Link from 'umi/link';
 
-export default class Home extends React.Component {
+export default class Index extends React.Component {
   render() {
     return (
       <div className={styles.container}>
-        <Link to="/category"><h2>分类管理-搜索模块 【店铺详情】</h2></Link>
-        <Link to="/home"><h2>首页【支付订单流程】</h2></Link>
-        <Link to="/product"><h2>商品模块 【评论模块</h2></Link>
-        <Link to="/limitbuy"><h2>限时秒杀 【地址管理模块】</h2></Link>
+        <Link to="/category"><div className={styles.item}>分类管理-搜索模块 【店铺详情】</div></Link>
+        <Link to="/home"><div className={styles.item}>首页【支付订单流程】</div></Link>
+        <Link to="/product"><div className={styles.item}>商品模块 【评论模块</div></Link>
+        <Link to="/limitbuy"><div className={styles.item}>限时秒杀 【地址管理模块】</div></Link>
       </div>
     );
   }
