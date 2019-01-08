b9b3693d96849977dd127fe90a7916c2c7218970
love1234 <345841634@qq.com>
2019-01-08 17:10:52 +0800

    xxx

--- src/pages/limitbuy/index.js
+++ src/pages/limitbuy/index.js
@@ -1,13 +1,31 @@
 import React, { PureComponent } from 'react';
 import styles from './index.less';
 import { TabBar } from 'antd-mobile';
+import Swiper from 'react-id-swiper';
 
 export default class Index extends PureComponent {
 
   render() {
+    const params = {
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
+      }
+    }
+
+
     return (
-      <div>
-        限时秒杀
+      <div className={styles.container}>
+        <div>
+          <Swiper {...params}>
+            <div >Slide 1</div>
+            <div >Slide 1</div>
+            <div >Slide 1</div>
+            <div >Slide 1</div>
+          </Swiper>
+        </div>
       </div>
     );
   }
