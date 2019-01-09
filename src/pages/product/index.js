341f221725fd055d1ab33df7b21147c05440b135
love1234 <345841634@qq.com>
2019-01-09 14:31:29 +0800

    xx

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,13 +1,41 @@
 import React, { PureComponent } from 'react';
 import styles from './index.less';
-import { TabBar } from 'antd-mobile';
+import { Tabs, WhiteSpace, Badge } from 'antd-mobile';
+const tabs = [
+  { title: <Badge text={'3'}>First Tab</Badge> },
+  { title: <Badge text={'今日(20)'}>Second Tab</Badge> },
+  { title: <Badge dot>Third Tab</Badge> },
+];
+
+const tabs2 = [
+  { title: 'First Tab', sub: '1' },
+  { title: 'Second Tab', sub: '2' },
+  { title: 'Third Tab', sub: '3' },
+];
+
 
 export default class Index extends PureComponent {
 
   render() {
     return (
-      <div>
-        商品详情
+      <div style={{height:'100%',position:'absolute'}}>
+        <Tabs tabs={tabs}
+              tabBarPosition="top"
+              initialPage={1}
+              onChange={(tab, index) => { console.log('onChange', index, tab); }}
+              onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
+        >
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+            Content of first tab
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+            Content of second tab
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+            Content of third tab
+          </div>
+        </Tabs>
+        <WhiteSpace />
       </div>
     );
   }
