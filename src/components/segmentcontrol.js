b76ae30b8651cbca1886fa02d3cd9d050257ccbe
love1234 <345841634@qq.com>
2019-01-11 15:54:41 +0800

    xxx

---
+++ src/components/segmentcontrol.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./index.less";
+
+/**
+ *日期: 2019-01-11
+ *作者: lovecross
+ *功能:
+ */
+export default class SegmentedControl extends React.Component {
+
+  state = {
+    activeIndex: 0
+  };
+
+  render() {
+    const { values } = this.props;
+    const activeIndex = this.state.activeIndex;
+    const firstIndex = 0;
+    const lastIndex = values.length - 1;
+    return <div className={styles.segmentcontrol}>
+      {
+        values.map((title, index) => {
+          if (index === firstIndex) return <div onClick={() => this.setState({ activeIndex: index })} key={index + "#"}
+                                                className={activeIndex === index ? styles.segmentcontrol_left_active : styles.segmentcontrol_left}>{title}</div>;
+          if (index === lastIndex) return <div onClick={() => this.setState({ activeIndex: index })} key={index + "#"}
+                                               key={index + "#"}
+                                               className={activeIndex === index ? styles.segmentcontrol_right_active : styles.segmentcontrol_right}>{title}</div>;
+          return <div onClick={() => this.setState({ activeIndex: index })} key={index + "#"} key={index + "#"}
+                      className={activeIndex === index ? styles.segmentcontrol_item_active : styles.segmentcontrol_item}>{title}</div>;
+          ;
+        })
+      }
+    </div>;
+  }
+};
--- src/pages/limitbuy/index.js
+++ src/pages/limitbuy/index.js
@@ -8,6 +8,7 @@ import shoppingcart_btn from "./image/goodtea_btn_shopping@2x.png";
 
 import router from "umi/router";
 import ModalBox from "../../components/modal";
+import SegmentedControl from "../../components/segmentcontrol";
 
 const Header = () => {
   return (
@@ -53,33 +54,6 @@ const Products = ({ onClick,addToShoppingCart }) => {
   </div>;
 };
 
-class SegmentedControl extends React.Component {
-
-  state = {
-    activeIndex: 0
-  };
-
-  render() {
-    const { values } = this.props;
-    const activeIndex = this.state.activeIndex;
-    const firstIndex = 0;
-    const lastIndex = values.length - 1;
-    return <div className={styles.segmentcontrol}>
-      {
-        values.map((title, index) => {
-          if (index === firstIndex) return <div onClick={() => this.setState({ activeIndex: index })} key={index + "#"}
-                                                className={activeIndex === index ? styles.segmentcontrol_left_active : styles.segmentcontrol_left}>{title}</div>;
-          if (index === lastIndex) return <div onClick={() => this.setState({ activeIndex: index })} key={index + "#"}
-                                               key={index + "#"}
-                                               className={activeIndex === index ? styles.segmentcontrol_right_active : styles.segmentcontrol_right}>{title}</div>;
-          return <div onClick={() => this.setState({ activeIndex: index })} key={index + "#"} key={index + "#"}
-                      className={activeIndex === index ? styles.segmentcontrol_item_active : styles.segmentcontrol_item}>{title}</div>;
-          ;
-        })
-      }
-    </div>;
-  }
-};
 
 const Timer = () => {
   return <div className={styles.timer}>
