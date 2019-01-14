ac54db9b99a420f678773fac0b28e957a972d051
love1234 <345841634@qq.com>
2019-01-14 11:03:17 +0800

    xxx

---
+++ src/pages/me/index.js
@@ -0,0 +1,17 @@
+/**
+ *日期: 2019-01-14
+ *作者: lovecross
+ *功能:
+ */
+
+import React, { PureComponent } from "react";
+import styles from "./index.less";
+
+export default class Me extends PureComponent{
+  render() {
+    return <div>
+      我
+    </div>
+  }
+}
+
