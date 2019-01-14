c0d57b5d80b0547bf32cfe9dfe0a126cc268950f
love1234 <345841634@qq.com>
2019-01-14 13:49:28 +0800

    xxx

--- src/pages/limitbuy/index.js
+++ src/pages/limitbuy/index.js
@@ -4,6 +4,7 @@ import { TabBar } from "antd-mobile";
 import Swiper from "react-id-swiper";
 import header_bg from "./image/goodtea_top_bg@2x.png";
 import shoppingcart_btn from "./image/goodtea_btn_shopping@2x.png";
+import ScrollWrap from "../../components/scroll";
 
 import router from "umi/router";
 import ModalBox from "../../components/modal";
@@ -22,7 +23,7 @@ const Header = () => {
 const ProductItem = ({ onClick, addToShoppingCart }) => {
   return (
     <div className={styles.product_item}>
-      <img className={styles.product_item_icon} src="" alt="" />
+      <img className={styles.product_item_icon} src="" alt=""/>
       <div className={styles.product_item_infos}>
         <div className={styles.product_item_title} onClick={onClick}>
           蜜桃乌龙茶花果茶买2送1 白桃乌龙茶15包组
@@ -35,7 +36,7 @@ const ProductItem = ({ onClick, addToShoppingCart }) => {
         </div>
       </div>
       <div onClick={addToShoppingCart}>
-        <img className={styles.shopping_btn} src={shoppingcart_btn} alt="" />
+        <img className={styles.shopping_btn} src={shoppingcart_btn} alt=""/>
       </div>
     </div>
   );
@@ -44,11 +45,11 @@ const ProductItem = ({ onClick, addToShoppingCart }) => {
 const Products = ({ onClick, addToShoppingCart }) => {
   return (
     <div>
-      <ProductItem addToShoppingCart={addToShoppingCart} onClick={onClick} />
-      <ProductItem onClick={onClick} />
-      <ProductItem onClick={onClick} />
-      <ProductItem onClick={onClick} />
-      <ProductItem onClick={onClick} />
+      <ProductItem addToShoppingCart={addToShoppingCart} onClick={onClick}/>
+      <ProductItem onClick={onClick}/>
+      <ProductItem onClick={onClick}/>
+      <ProductItem onClick={onClick}/>
+      <ProductItem onClick={onClick}/>
     </div>
   );
 };
@@ -122,6 +123,12 @@ const Timer = () => {
 };
 
 export default class Index extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.clientHeight = window.document.body.clientHeight;
+  }
+
   state = {
     modalVisible: false
   };
@@ -139,63 +146,28 @@ export default class Index extends PureComponent {
   };
 
   render() {
-    const params = {
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        type: "bullets"
-      },
-      containerClass: "limit_swiper_container"
-    };
-
     return (
       <div className={styles.container}>
-        <Header />
-        <SegmentedControl values={["今日秒杀", "明日预告"]} />
-        <Timer />
-        <Products
-          addToShoppingCart={() => {
-            this.openModal();
+        <div
+          style={{
+            height: `${this.clientHeight}px`,
+            position: "relative"
           }}
-          onClick={() => {
-            router.push("/product");
-          }}
-        />
-        <ModalBox visible={this.state.modalVisible} onClose={this.onModalClose}>
-          <div>
-            <div className={styles.model_content}>
-              <div className={styles.model_flex_r}>
-                <div className={styles.modal_p_img_wrapper}>
-                  <img
-                    src="http://img4.imgtn.bdimg.com/it/u=2769118404,1000928488&fm=15&gp=0.jpg"
-                    alt=""
-                    className={styles.modal_p_img}
-                  />
-                </div>
-                <div className={styles.modal_p_info}>
-                  <div className={styles.modal_price}>￥123</div>
-                  <div className={styles.modal_p_name}>
-                    2018新茶西湖牌龙井茶叶正宗雨前西湖龙井茶250g
-                  </div>
-                </div>
-              </div>
-              <div className={styles.standard}>规格</div>
-            </div>
-
-            <div>xxxxxxx</div>
-            <div>xxxxxxx</div>
-            <div>xxxxxxx</div>
-            <div>xxxxxxx</div>
-
-            <div
-              onClick={this.onModalClose}
-              className={styles.modal_footer_btn}
-            >
-              确定
-            </div>
-          </div>
-        </ModalBox>
+        >
+          <ScrollWrap wrapId="lefttList" wrapClass={styles.wrap_body}>
+            <Header/>
+            <SegmentedControl values={["今日秒杀", "明日预告"]}/>
+            <Timer/>
+            <Products
+              addToShoppingCart={() => {
+                this.openModal();
+              }}
+              onClick={() => {
+                router.push("/product");
+              }}
+            />
+          </ScrollWrap>
+        </div>
       </div>
     );
   }
