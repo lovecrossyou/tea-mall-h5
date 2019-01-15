8762da2e22bf88fb4f6b4f302a8da4f58164a52e
love1234 <345841634@qq.com>
2019-01-15 17:34:43 +0800

    xx

---
+++ mock/shoppingcart.js
@@ -0,0 +1,42 @@
+/**
+ *日期: 2019-01-15
+ *作者: lovecross
+ *功能:
+ */
+
+export default {
+  "POST /h5/shoppingcart/list": {
+    code: 0,
+    list: [
+      {
+        shopName:'张一元旗舰店',
+        products:[{
+          pName:'洞庭碧螺春洞庭碧螺春洞庭碧螺春',
+          summary:'规格6小包80g',
+          price:'￥500',
+          count:6
+        },{
+          pName:'洞庭碧螺春洞庭碧螺春洞庭碧螺春',
+          summary:'规格6小包80g',
+          price:'￥500',
+          count:6
+        }]
+      },
+      {
+        shopName:'吴裕泰旗舰店',
+        products:[{
+          pName:'洞庭碧螺春洞庭碧螺春洞庭碧螺春',
+          summary:'规格6小包80g',
+          price:'￥500',
+          count:6
+        },{
+          pName:'洞庭碧螺春洞庭碧螺春洞庭碧螺春',
+          summary:'规格6小包80g',
+          price:'￥500',
+          count:6
+        }]
+      }
+    ]
+  }
+};
+
